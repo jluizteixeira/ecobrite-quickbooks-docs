8ed11ff47b38832e319f75baa248efdae3c09108
jluizteixeira <111467890+jluizteixeira@users.noreply.github.com>
2025-05-19 15:16:39 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, request
+import os
 
 app = Flask(__name__)
 
@@ -12,4 +13,5 @@ def callback():
     return f"Code: {code}<br>Realm ID: {realm_id}<br>State: {state}"
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=10000)
+    port = int(os.environ.get('PORT', 10000))
+    app.run(host='0.0.0.0', port=port)
