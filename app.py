b99a0ec5ff5be82c4826fb9fd9538d7ea679aa39
jluizteixeira <111467890+jluizteixeira@users.noreply.github.com>
2025-05-19 15:14:46 -0300

    app.py

--- app.py
+++ app.py
@@ -5,11 +5,11 @@ app = Flask(__name__)
 @app.route('/callback')
 def callback():
     code = request.args.get('code')
+    realm_id = request.args.get('realmId')
     state = request.args.get('state')
-    if code:
-        return f"OAuth callback recebido com sucesso! Código: {code}"
-    else:
-        return "Nenhum código OAuth encontrado no callback.", 400
+    
+    # Simplesmente para debug agora — depois você faz o token exchange
+    return f"Code: {code}<br>Realm ID: {realm_id}<br>State: {state}"
 
 if __name__ == '__main__':
-    app.run()
+    app.run(host='0.0.0.0', port=10000)
